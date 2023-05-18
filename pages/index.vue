8a85574f3c98dfe88eaf57ba169f385215387304
ThandarAungUCSM <thandaraung903@gmail.com>
2023-05-18 09:20:31 +0630

    sidebar front page

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,81 @@
   <div>
     <!-- @checkAuth="checkAuth" -->
     <!-- page="prodDetail" // sidebar bugs -->
-    <Header />
+    <!-- <Header /> -->
+
     <div class="content-css">
-      
+      <div class="content-block">
+        <div v-if="opensidebar" class="sidebar-open" >
+          <div class="img-row">
+            <img src="../assets/pc/sidebar-close.png" class="clse-img" @click="sidebarFunc">
+            <img src="../assets/pc/sidebar-logout.png" class="logout-img">
+          </div>
+          <div>
+            <div class="pk-admin">
+              <p class="pk-title">PK 管理後台 Admin</p>
+            </div>
+            <div class="each-pk">
+              <p class="each-title">平台管理</p>
+              <div class="each-row">
+                <img src="../assets/pc/close-dollar.png" class="sideopen-img">
+                <span class="dollar-text">幣值比率管理</span>
+              </div>
+              <div class="each-row">
+                <img src="../assets/pc/close-manager.png" class="sideopen-img">
+                <span class="dollar-text">橫幅廣告設定</span>
+              </div>
+              <div class="each-row">
+                <img src="../assets/pc/close-cart.png" class="sideopen-img">
+                <span class="dollar-text">上架商品管理</span>
+              </div>
+              <div class="each-row">
+                <img src="../assets/pc/close-coupon.png" class="sideopen-img">
+                <span class="dollar-text">優惠券設定</span>
+              </div>
+            </div>
+            <div class="each-pk">
+              <p class="each-title">會員管理</p>
+              <div class="each-row">
+                <img src="../assets/pc/close-info.png" class="sideopen-img">
+                <span class="dollar-text">會員資料與連動遊戲</span>
+              </div>
+              <div class="each-row">
+                <img src="../assets/pc/close-receipt.png" class="sideopen-img">
+                <span class="dollar-text">會員兌換記錄查詢</span>
+              </div>
+              <div class="each-row">
+                <img src="../assets/pc/close-vector.png" class="sideopen-img">
+                <span class="dollar-text">會員退貨審核管理</span>
+              </div>
+              <div class="each-row">
+                <img src="../assets/pc/close-document.png" class="sideopen-img">
+                <span class="dollar-text">對帳資料</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-else class="sidebar-close" >
+          <img src="../assets/pc/sidebar-menu.png" class="menu-img" @click="sidebarFunc">
+          <div class="img-column">
+            <img src="../assets/pc/close-dollar.png" class="sideclose-img" >
+            <img src="../assets/pc/close-manager.png" class="sideclose-img" >
+            <img src="../assets/pc/close-cart.png" class="sideclose-img" >
+            <img src="../assets/pc/close-coupon.png" class="sideclose-img" >
+            <img src="../assets/pc/close-info.png" class="sideclose-img" >
+            <img src="../assets/pc/close-receipt.png" class="sideclose-img" >
+            <img src="../assets/pc/close-vector.png" class="sideclose-img" >
+            <img src="../assets/pc/close-document.png" class="sideclose-img" >
+          </div>
+        </div>
+        <div class="whole-content" :class="opensidebar ? 'opentrue' : 'openfalse'">
+          <div class="page-content" >
+            
+          </div>
+        </div>
+      </div>
     </div>
 
-    <Footer page="front" />
+    <!-- <Footer /> -->
   </div>
 </template>
 
@@ -17,7 +86,8 @@ export default {
   name: 'IndexPage',
   data() {
     return {
-      userLogin: false
+      userLogin: false,
+      opensidebar: true
     }
   },
   computed: {
@@ -25,6 +95,9 @@ export default {
   methods: {
     checkAuth(auth) {
       this.userLogin = auth
+    },
+    sidebarFunc() {
+      this.opensidebar = !this.opensidebar;
     }
   }
 }
@@ -34,5 +107,114 @@ export default {
 
 </style>
 <style lang="scss" scoped>
+.content-css {
+  height: 100vh;
+  min-height: 100vh ;
+  background: #132235;
+  .content-block {
+    display: flex;
+    position: relative;
+    .sidebar-open {
+      background: #191A21;
+      border-radius: 0 24px 24px 0;
+      width: 319px;
+      height: 100vh;
+      z-index: 10;
+      padding: 0 1rem 0 44px;
+      .img-row {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 36px;
+        margin-bottom: 100px;
+        padding: 0;
+        .clse-img {
+          width: 24px;
+          height: 24px;
+          cursor: pointer;
+        }
+        .logout-img {
+          width: 24px;
+          height: 24px;
+        }
+      }
+      .pk-admin {
+        .pk-title {
+          font-weight: 700;
+          font-size: 1rem;
+          color: #7161EF;
+          margin-bottom: 0;
+        }
+      }
+      .each-pk {
+        margin-top: 2rem;
+        .each-title {
+          font-weight: 400;
+          font-size: 16px;
+          color: #FFF;
+        }
+        .each-row {
+          display: flex;
+          align-items: center;
+          margin-bottom: 1.3rem;
+          .sideopen-img {
+            width: 24px;
+            height: 24px;
+            margin-right: 1rem;
+          }
+          .dollar-text {
+            font-weight: 500;
+            font-size: 20px;
+            color: #06A2FF;
+          }
+        }
+      }
+    }
+    .sidebar-close {
+      background: #191A21;
+      width: 109px;
+      height: 100vh;
 
+      text-align: center;
+      padding-top: 36px;
+      z-index: 10;
+      .menu-img {
+        width: 24px;
+        height: 24px;
+        cursor: pointer;
+      }
+      .img-column {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 120px;
+        .sideclose-img {
+          width: 24px;
+          height: 24px;
+
+          margin-bottom: 2rem;
+        }
+      }
+    }
+    .whole-content {
+      width: 100%;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, 0%); 
+    }
+    .page-content {
+      background: #7161EF;
+      border-radius: 36px;
+      width: 40%;
+      height: 261px;
+      margin: 32px auto 0;
+    }
+    .opentrue {
+      padding-left: 20%; // after open sidebar
+    }
+    .openfalse {
+      padding-left: 109px;
+    }
+  }
+}
 </style>
